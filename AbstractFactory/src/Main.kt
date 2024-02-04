cc33261d85ee6fd0d7e8635b5baf6875bcabfbfa
colgate116 <shavrin1609@gmail.com>
2024-02-04 21:26:22 +0300

    1

---
+++ AbstractFactory/src/Main.kt
@@ -0,0 +1,22 @@
+fun main() {
+    while (true){
+        val color = readln().toInt()
+        val unit = readln().toInt()
+        createFactory(color, unit).attack()
+    }
+}
+
+fun createFactory(color: Int, unit: Int): Unit{
+    val factory: Factory = when (color){
+        1 -> GreenUnitsFactory()
+        2 -> RedUnitsFactory()
+        3 -> BlueUnitsFactory()
+        else -> throw Exception()
+    }
+    return when (unit){
+        1 -> factory.createMage()
+        2 -> factory.createKnight()
+        3 -> factory.createBeast()
+        else -> throw Exception()
+    }
+}
