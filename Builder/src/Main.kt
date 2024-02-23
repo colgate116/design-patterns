1431dd61ac6fede165bbac10af356043d2c54571
colgate116 <shavrin1609@gmail.com>
2024-02-23 14:11:45 +0300

    Builder pattern 1

--- Builder/src/Main.kt
+++ Builder/src/Main.kt
@@ -2,5 +2,4 @@ fun main() {
     val unit1: Unit = UntBuilder(1).buildUnit("1")
     val unit2: Unit = UntBuilder(2).buildUnit("2")
     val unit3: Unit = UntBuilder(3).buildUnit("3")
-    //запуш
 }
