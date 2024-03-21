f649e23e03d55c59602e4c99e2c03696b3b6bcce
colgate116 <shavrin1609@gmail.com>
2024-03-22 00:10:58 +0300

    vector

--- untitled/src/Main.kt
+++ untitled/src/Main.kt
@@ -13,32 +13,32 @@ fun main() {
 
     for (i in 0..15)
         array.removeLast()
-
     array.plus(4)
     println(array)
+    println(array.size)
 
 }
 
-class Vector(private var size: Int = 2){
-    private var vector = IntArray(size)
-    private var currentLength = 0
+class Vector(private var capacity: Int = 2){
+    private var vector = IntArray(capacity)
+    var size = 0
 
     private fun capacityIncrease(){
-        currentLength++
-        if (currentLength>size){
-            size*=2
-            vector = vector.copyOf(size)
+        size++
+        if (size>capacity){
+            capacity*=2
+            vector = vector.copyOf(capacity)
         }
     }
 
     private fun indexValidation(index: Int){
-        if (index>currentLength || index<0)
+        if (index>size || index<0)
             throw IndexOutOfBoundsException()
     }
 
     fun plus(value: Int){
         capacityIncrease()
-        vector[currentLength-1] = value
+        vector[size-1] = value
     }
 
     fun get(index: Int): Int{
@@ -56,7 +56,7 @@ class Vector(private var size: Int = 2){
         indexValidation(index)
         capacityIncrease()
         var el: Int = vector[index]
-        for (i in index..currentLength-2){
+        for (i in index..size-2){
             val el1 = vector[i+1]
             vector[i+1] = el
             el = el1
@@ -65,16 +65,16 @@ class Vector(private var size: Int = 2){
     }
 
     fun removeLast(){
-        currentLength--
-        if (currentLength>0)
-            vector[currentLength-1] = vector[currentLength]
+        size--
+        if (size>0)
+            vector[size-1] = vector[size]
     }
 
     fun remove(index: Int){
         indexValidation(index)
-        currentLength--
-        var el: Int = vector[currentLength]
-        for (i in currentLength downTo index+1){
+        size--
+        var el: Int = vector[size]
+        for (i in size downTo index+1){
             val el1 = vector[i-1]
             vector[i-1] = el
             el = el1
@@ -84,9 +84,9 @@ class Vector(private var size: Int = 2){
 
     override fun toString(): String {
         //вывод относительно логичского размера массива
-        //return vector.slice(0..<currentLength).joinToString()
+        return vector.slice(0..<size).joinToString()
 
     //вывод относительно физического размера массива
-        return vector.contentToString()
+        //return vector.contentToString()
     }
 }
